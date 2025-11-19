a1e381d341aa72fcececf69e4e4c899ade2d7cc5
MD_Vignesh <devivignesh160@gmail.com>
2025-11-19 19:41:11 +0530

    prg02
    Refactor EServer to improve client handling and add logging.

--- Prg2/EServer.java
+++ Prg2/EServer.java
@@ -1,13 +1,35 @@
+
 import java.io.*;
 import java.net.*;
 
 public class EServer {
-    public static void main(String[] args) throws Exception {
-        ServerSocket ss = new ServerSocket(9000);
-        Socket s = ss.accept();
-        BufferedReader in = new BufferedReader(new InputStreamReader(s.getInputStream()));
-        PrintWriter out = new PrintWriter(s.getOutputStream(), true);
+    public static void main(String args[]) {
+        ServerSocket s = null;
         String line;
-        while ((line = in.readLine()) != null) out.println(line);
+        DataInputStream is;
+        PrintStream ps;
+        Socket c = null;
+
+        try {
+            s = new ServerSocket(9000);
+            System.out.println("Server started. Waiting for client...");
+        } catch (IOException e) {
+            System.out.println(e);
+        }
+
+        try {
+            c = s.accept();
+            System.out.println("Client Connected!");
+
+            is = new DataInputStream(c.getInputStream());
+            ps = new PrintStream(c.getOutputStream());
+
+            while (true) {
+                line = is.readLine();      // receive from client
+                ps.println(line);          // send back to client (echo)
+            }
+        } catch (IOException e) {
+            System.out.println(e);
+        }
     }
 }
